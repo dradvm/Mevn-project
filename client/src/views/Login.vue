e43e0a310d5deee5d2599de3ae4b7d0b3fca24f1
Bich Ngoc <nbichngoc3904@gmail.com>
2024-07-08 00:24:47 +0700

    Sơ sơ vị hui

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,135 +1,165 @@
 <template>
 <div class="login-main">
-
 	<div class="main">  	
 		<input type="checkbox" id="chk" aria-hidden="true">
 
-			<div class="signup">
-				<form>
-					<label for="chk" aria-hidden="true">Sign up</label>
-					<input type="email" name="email" placeholder="Email" required="">
-                    <input type="String" name="phone" placeholder="Phone Number" required="">
-					<input type="password" name="pswd" placeholder="Password" required="">
-					<RouterLink to="/register" style="text-decoration: none;"><button>Continue</button></RouterLink>
+		<div class="login">
+				<form class="vstack gap">
+                    <div class="title">Login</div>
+                    <div class="form">
+                        <p for="name">Email:</p>
+                        <input type="email" name="email" required="">
+                        
+                        <p for="name">Password:</p>
+                        <input type="password" name="pswd" required="">
+                    </div>
+					<div class="d-flex justify-content-around">
+						<button>Login</button>
+					</div>
+					
+					<div class="bottom">
+						<span class="forget-pass">Forget password</span>
+                    	<span class="form-tip"><RouterLink to="/register">Sign up</RouterLink></span>
+					</div>
+					
 				</form>
-			</div>
-
-			<div class="login">
-				<form>
-					<label for="chk" aria-hidden="true">Login</label>
-					<input type="email" name="email" placeholder="Email" required="">
-					<input type="password" name="pswd" placeholder="Password" required="">
-					<button class="login-but">Login</button>
-				</form>
-			</div>
+        </div>
 	</div>
 </div>
-
 </template>
 
+
 <style scoped>
+:root {
+    --button-hover: #1261A6;
+    --button: #2a95bf;
+    --pastel: #73C6D9;
+    --dark-blue: #126DA6;
+    --grey: #5c6677;
+    --navy: #23395d;
+	--text: #021534;
+}
 .login-main{
 	margin: 0;
 	padding: 0;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	min-height: 100vh;
+	height: 100vh;
 	font-family: 'Jost', sans-serif;
-	background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
+	background: #0c86d19a;
 }
 .main{
-	width: 350px;
-	height: 500px;
-	background: red;
+	width: 600px;
+	height: 400px;
+	background: #2a95bf;
 	overflow: hidden;
-	background: linear-gradient(to right, #6ee2f5, #6454f0);
 	border-radius: 10px;
 	box-shadow: 5px 20px 50px #000;
+}
+.title{
+	color: #021534;
+	font-size: 2.5em;
+	justify-content: center;
+	display: flex;
+	font-weight: bold;
+	cursor: pointer;
+	margin: 20px auto;
+	text-shadow: 2px 2px 3px rgba(124, 158, 221, 0.695);
 }
 #chk{
 	display: none;
 }
-.signup{
-	position: relative;
-	width:100%;
-	height: 100%;
-}
-label{
-	color: #fff;
-	font-size: 2.3em;
-	justify-content: center;
-	display: flex;
-	margin: 50px;
-	font-weight: bold;
-	cursor: pointer;
-	transition: .5s ease-in-out;
-}
 input{
 	width: 60%;
 	height: 35px;
-	background: rgb(180, 206, 243);
+	background: rgba(124, 158, 221, 0.695);
 	justify-content: center;
 	display: flex;
 	margin: 15px auto;
-	padding: 10px;
+	padding: 10px 20px;
 	border: none;
 	outline: none;
 	border-radius: 5px;
+	margin: 30px auto;
 }
 
-
 button{
-	width: 60%;
-	height: 40px;
-	margin: 10px auto;
+	align-items: center;
+	width: 130px;
+	height: 45px;
 	justify-content: center;
-	display: block;
+	display: flex;
 	color: #fff;
 	background: #0c85d1;
-	font-size: 1em;
+	font-size: 1.5em;
 	font-weight: bold;
-	margin-top: 20px;
 	outline: none;
 	border: none;
 	border-radius: 5px;
 	transition: .2s ease-in;
 	cursor: pointer;
-}
-.login-but{
-    background: #076eae;
+	
 }
 button:hover{
-	background: #41adf0;
-}
-.login{
-	height: 460px;
-	background: #0c85d1;
-	border-radius: 60% / 10%;
-	transform: translateY(-180px);
-	transition: .8s ease-in-out;
-}
-.login label{
-	color: #ffffff;
-	transform: scale(.6);
+	background: #126DA6;
 }
 
-#chk:checked ~ .login{
-	transform: translateY(-500px);
+p{
+    font-size: 1em;
+    color: #021534;
+	width: 100%;
+	height: 5px;
+	display: flex;
+	justify-content: left;
+	margin-left: 130px;
 }
-#chk:checked ~ .login label{
-	transform: scale(1);	
-}
-#chk:checked ~ .signup label{
-	transform: scale(.6);
-}
+.bottom {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 
+  }
+  
+  .forget-pass {
+	font-size: 14px;
+	margin-top: 5px;
+  }
+  .forget-tip {
+	font-size: 14px;
+  }
 </style>
 
+
+ 
 <script>
-export default {
-    name: "Login"
+function login(userNameOrEmail, password, callback) {
+  // Send credentials to external database API
+  let hashedPassword = hash(password);
+
+  let options = {
+    url: "https://example.com/api/authenticate",
+    body: {
+      email: userNameOrEmail,
+      password: hashedPassword
+    }
+  };
+
+  send(options, (err, profileData) => {
+    // Return error in callback if authentication is unsuccessful
+    if (err) {
+      return callback(new WrongUsernameOrPasswordError(userNameOrEmail, "My custom error message."));
+    } else {
+      // Return profile data in callback if authentication is successful
+      let profile = {
+        username: profileData.username,
+        email: profileData.emailAddress,
+        user_id: profileData.userId
+      };
+
+      return callback(null, profile);
+    }
+  });
 }
-
-
 </script>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,131 +1,221 @@
 <template>
-<div class="login-main">
-	<div class="main">  
-		<input type="checkbox" id="chk" aria-hidden="true">
+  <div class="register-main">
+    <div class="main">  	
+      <input type="checkbox" id="chk" aria-hidden="true">
+      <div class="login">
 
-    
-        <div class="signup">
-            <form>
-                <div class="sign-up">Sign up</div>
-                <div class="Username">
-                    <p for="name">User name</p>
-                    <input type="name" name="name" placeholder="Full name" required="">
-                </div>
-                
-                
-                <div class="input-birthday">
-                    <span class="date-input">
-                        <span class="date-label">Birthday</span>
-                        <input type="date" id="birthday" name="birthday">
+        <div class="title">Sign up</div>
+        <div class="content">
+          <form action="#" class="vstack gap">
+            
+            <div class="user-details">
 
-                    </span>
-                  </div>
+              <div class="input-two-col">
+                <span class="details">Full name:</span>
+                <input type="text" name="name" placeholder="Enter your full name" required>
+              </div>
 
-                <input type="String" name="address" placeholder="Address" required="">
-                <button>Sign up</button>
-            </form>
+              <div class="input-two-col">
+                  <span class="details">Birthday:</span>
+                  <input type="date" name="birthday">
+              </div>
+
+              <div class="gender-details">
+                <span class="details">Gender:</span>
+                <select id="gender" name="gender">
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+
+              <div class="email-col">
+                <span class="details">Email:</span>
+                <input type="text" name="email" placeholder="Enter your email" required>
+              </div>
+
+              <div class="phone-col">
+                <span class="details">Phone number:</span>
+                <input type="text" name="phone" placeholder="Enter your number" required>
+              </div>
+
+
+
+              <div class="input-two-col">
+                <span class="details">Password</span>
+                <input type="text" placeholder="Enter your password" required>
+              </div>
+
+              <div class="input-two-col">
+                <span class="details">Confirm Password</span>
+                <input type="text" placeholder="Confirm your password" required>
+              </div>
+              <div class="input-two-col">
+                <span class="details">Address:</span>
+                <input type="text" name="address" placeholder="Enter your address" required>
+              </div>
+              
+            </div>
+
+            <button class="login-but">Sign up</button>
+          </form>
+            
         </div>
-    </div>
-</div>
-</template>
 
+      </div>
+    </div>
+  </div>
+</template>
+    
+    
 <style scoped>
-.login-main{
-	margin: 0;
-	padding: 0;
-	display: flex;
+    :root {
+        --button-hover: #1261A6;
+        --button: #2a95bf;
+        --pastel: #73C6D9;
+        --dark-blue: #126DA6;
+        --grey: #5c6677;
+        --navy: #23395d;
+      }
+    .register-main{
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      font-family: 'Jost', sans-serif;
+      background: #0c86d19a;
+    }
+    .main{
+      width: 600px;
+      height: 400px;
+      background: #2a95bf;
+      overflow: hidden;
+      border-radius: 10px;
+      box-shadow: 5px 20px 50px #000;
+    }
+
+    .title{
+	color: #021534;
+	font-size: 2.5em;
 	justify-content: center;
-	align-items: center;
-	min-height: 100vh;
-	font-family: 'Jost', sans-serif;
-	background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
-}
-.main{
-	width: 350px;
-	height: 500px;
-	background: red;
-	overflow: hidden;
-	background: linear-gradient(to right, #6ee2f5, #6454f0);
-	border-radius: 10px;
-	box-shadow: 5px 20px 50px #000;
-}
-#chk{
-	display: none;
-}
-.signup{
-	position: relative;
-	width:100%;
-	height: 100%;
-}
-.sign-up{
-	color: #fff;
-	font-size: 2.3em;
-	justify-content: center;
 	display: flex;
-	margin: 50px;
-    margin-bottom: 20px;
 	font-weight: bold;
 	cursor: pointer;
-	transition: .5s ease-in-out;
-}
+	margin: 20px auto;
+	text-shadow: 2px 2px 3px rgba(124, 158, 221, 0.695);
+    }
+    #chk{
+      display: none;
+    }
+
+    
+    .user-details {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 10px;
+      margin: 10px 20px;
+    }
+    
+    .input-two-col {
+      grid-column: span 1;
+    }
+    
+    .input-one-col {
+      grid-column: span 1;
+    }
+    
+    .email-col {
+      grid-column: span 2;
+    }
+    
+    .phone-col {
+      grid-column: span 1;
+    }
+    
+    .gender-details {
+      grid-column: span 1;
+    }
+    
+    @media (max-width: 768px) {
+      .user-details {
+        grid-template-columns: 1fr;
+      }
+    
+      .input-two-col,
+      .input-one-col,
+      .email-col,
+      .phone-col,
+      .gender-details {
+        grid-column: span 1;
+      }
+    }
+
 input{
-	width: 60%;
-	height: 35px;
-	background: rgb(180, 206, 243);
-	justify-content: center;
-	display: flex;
-	margin: 15px auto;
-	padding: 10px;
-	border: none;
-	outline: none;
-	border-radius: 5px;
-}
-.input-birthday {
-    align-items: center;
-    width: 60%;
-	height: 35px;
-	background: rgb(180, 206, 243);
-	justify-content: center;
-	display: flex;
-	margin: 15px auto;
-	padding: 10px;
-	border: none;
-	outline: none;
-	border-radius: 5px;
+      width: 100%;
+      height: 35px;
+      background: rgba(124, 158, 221, 0.695);
+      justify-content: center;
+      display: flex;
+      margin: 10px 20px;
+      padding: 10px 20px;
+      border: none;
+      outline: none;
+      border-radius: 5px;
   }
-  
-  .date-input {
+  form .input-two-col span.details{
+      font-size: 1em;
+      color: #021534;
+      width: 100%;
+      height: 5px;
+      display: flex;
+      margin: 5px auto;
+      padding: 10px 20px;
+  }
+  form .input-one-col span.details{
+    font-size: 1em;
+    color: #021534;
+    width: 100%;
+    height: 5px;
     display: flex;
-    align-items: center;
-  }
-  
-  .date-label {
-    margin-left: 5px;
-  }
-input[type="email1"] {
-  background: rgba(61, 116, 164, 0.521);
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
+    margin: 5px auto;
+    padding: 10px 20px;
 }
-input[type="password1"] {
-  background: rgba(61, 116, 164, 0.521);
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
+form .gender-details span.details{
+  font-size: 1em;
+  color: #021534;
+  width: 100%;
+  height: 5px;
+  display: flex;
+  margin: 5px auto;
+  padding: 10px 20px;
+}
+form .gender-details #gender{
+  width: 100%;
+  height: 35px;
+  background: rgba(124, 158, 221, 0.695);
+  justify-content: center;
+  display: flex;
+  margin: 10px 40px;
+  padding: 10px 20px;
+  border: none;
+  outline: none;
+  border-radius: 5px;
 }
 
 button{
-	width: 60%;
-	height: 40px;
-	margin: 10px auto;
+	width: 130px;
+	height: 45px;
+	margin: 20px auto;
 	justify-content: center;
 	display: block;
 	color: #fff;
 	background: #0c85d1;
-	font-size: 1em;
+	font-size: 1.5em;
 	font-weight: bold;
-	margin-top: 20px;
+
+    margin-bottom: 50px;
 	outline: none;
 	border: none;
 	border-radius: 5px;
@@ -133,41 +223,190 @@ button{
 	cursor: pointer;
 }
 button:hover{
-	background: #41adf0;
+	background: #126DA6;
 }
+    /*.content form .user-details{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin: 5px 100px 5px 100px;
+      }
+      form .user-details .input-two-col{
+        width: calc(100% / 2 - 20px);
+      }
+      form .user-details .input-one-col{
+        width: 100%;
+      }
+    input{
+        width: 100%;
+        height: 35px;
+        background: rgba(124, 158, 221, 0.695);
+        justify-content: center;
+        display: flex;
+        margin: 10px auto 5px auto;
+        padding: 10px 20px;
+        border: none;
+        outline: none;
+        border-radius: 5px;
+    }
+    form .input-two-col span.details{
+        font-size: 1em;
+        color: #021534;
+        width: 100%;
+        height: 5px;
+        display: flex;
+        margin: 5px auto;
+        padding: 10px 20px;
+    }
+    form .input-one-col span.details{
+      font-size: 1em;
+      color: #021534;
+      width: 60%;
+      height: 5px;
+      display: flex;
+      margin: 5px auto;
+      padding: 10px 20px;
+  }
 
-p{
-    font-size: 0.8em;
-    color: rgb(0, 59, 110);
-	width: 60%;
-	height: 10px;
-	display: flex;
-	margin: 10px auto;
-	padding: 10px;
 
-}
-/*.login{
-	height: 460px;
-	background: gainsboro;
-	border-radius: 60% / 10%;
-	transform: translateY(-180px);
-	transition: .8s ease-in-out;
-}
-.login label{
-	color: #0c44d1;
-	transform: scale(.6);
-}
 
-#chk:checked ~ .login{
-	transform: translateY(-500px);
-}
-#chk:checked ~ .login label{
-    transform: scale(1);	
-}
-#chk:checked ~ .signup label{
-	transform: scale(.6);
-}*/
+       form .category label{
+         display: flex;
+         align-items: center;
+         cursor: pointer;
+       }
+       form .category label .dot{
+        height: 18px;
+        width: 18px;
+        border-radius: 50%;
+        margin-right: 10px;
+        background: #d9d9d9;
+        border: 5px solid transparent;
+        transition: all 0.3s ease;
+      }
+       #dot-1:checked ~ .category label .one,
+       #dot-2:checked ~ .category label .two,
+       #dot-3:checked ~ .category label .three{
+         background: #9b59b6;
+         border-color: #d9d9d9;
+       }
+       form input[type="radio"]{
+         display: none;
+       }
+       form .button{
+         height: 45px;
+         margin: 35px 0
+       }
+       button{
+        width: 130px;
+        height: 45px;
+        margin: 20px auto;
+        justify-content: center;
+        display: block;
+        color: #fff;
+        background: #0c85d1;
+        font-size: 1.5em;
+        font-weight: bold;
+      
+          margin-bottom: 50px;
+        outline: none;
+        border: none;
+        border-radius: 5px;
+        transition: .2s ease-in;
+        cursor: pointer;
+      }
+      button:hover{
+        background: #126DA6;
+      }
+       @media(max-width: 584px){
+       .container{
+        max-width: 100%;
+      }
+      form .user-details .input-box{
+          margin-bottom: 15px;
+          width: 100%;
+        }
+        form .category{
+          width: 100%;
+        }
+        .content form .user-details{
+          max-height: 300px;
+          overflow-y: scroll;
+        }
+        .user-details::-webkit-scrollbar{
+          width: 5px;
+        }
+        }
+        @media(max-width: 459px){
+        .container .content .category{
+          flex-direction: column;
+        }
+      }
+      #chk{
+        display: none;
+    }*/
 
+
+
+    /*.title{
+        color: #021534;
+        font-size: 2.5em;
+        justify-content: center;
+        display: flex;
+        margin: 50px;
+        margin-bottom: 5px;
+        font-weight: bold;
+        cursor: pointer;
+        text-shadow: 2px 2px 3px rgba(124, 158, 221, 0.695);
+    }
+    #chk{
+        display: none;
+    }
+    input{
+        width: 60%;
+        height: 35px;
+        background: rgba(124, 158, 221, 0.695);
+        justify-content: center;
+        display: flex;
+        margin: 10px auto 5px auto;
+        padding: 10px 20px;
+        border: none;
+        outline: none;
+        border-radius: 5px;
+    }
+      
+    button{
+        width: 25%;
+        height: 40px;
+        margin: 20px auto;
+        justify-content: center;
+        display: block;
+        color: #fff;
+        background: #0c85d1;
+        font-size: 1.2em;
+        font-weight: bold;
+    
+        margin-bottom: 50px;
+        outline: none;
+        border: none;
+        border-radius: 5px;
+        transition: .2s ease-in;
+        cursor: pointer;
+    }
+    button:hover{
+        background: #126DA6;
+    }
+    
+    p{
+        font-size: 1em;
+        color: #021534;
+        width: 60%;
+        height: 5px;
+        display: flex;
+        margin: 5px auto;
+        padding: 10px;
+    }*/
+    
 </style>
 
 <script>
