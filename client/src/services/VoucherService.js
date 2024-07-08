dd537a55dda18b29e883758d05e78d6835fadf14
dradvm <duynnvn2511@gmail.com>
2024-07-09 00:21:32 +0700

    sorting

--- client/src/services/VoucherService.js
+++ client/src/services/VoucherService.js
@@ -2,8 +2,20 @@ import createAxios from "@/utils/axios"
 const axios = createAxios("/voucher")
 const VoucherService = {
     getAll: () => axios.get(""),
-    getByPage: (page, search) => axios.get(`/page/${page}/${search}`),
-    getPages: (search) => axios.get(`/pages/${search}`)
+    getByPage: (page, search, check, sort) => axios.get(`/`, {
+        params: {
+            page,
+            search,
+            check,
+            sort
+        }
+    }),
+    getPages: (search, check) => axios.get(`/pages`, {
+        params: {
+            search,
+            check
+        }
+    })
 }
 
 export default VoucherService
--- client/src/views/MyShopVoucher.vue
+++ client/src/views/MyShopVoucher.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="m-3">
-        <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-between align-items-center">
             <div>
-                <button>Add new voucher</button>
+                <button class="px-3 py-1 fw-medium bg-primary rounded">Add new voucher</button>
             </div>
             <div>
                 <div class="my-input-group mb-3">
@@ -11,28 +11,52 @@
                 </div>
             </div>
         </div>
+        
+        <div class="">
+            <v-switch v-model="check" :model-value="check"
+                    hide-details
+                    :label="`Voucher ${labelSwitcher}available`"
+                    color="primary"
+                ></v-switch>
+        </div>
         <div class="rounded bg-light px-4 py-3 mt-3">
             <div class="px-3 fw-bold d-flex align-items-center justify-content-between text-center mb-3">
                 <div class="fw-bold" style="width: 10%">
                     Code
                 </div>
-                <div class="fw-bold" style="width: 20%">
-                    Date
+                <div class="d-flex justify-content-around" style="width: 20%">
+                    <div class="d-flex align-items-center fw-bold cur-p px-2 py-1 sortItem" @click="toggleSort('startDateSort')">
+                        Date
+                        <font-awesome-icon v-if="startDateSort" class="ms-1 sortIcon" style="margin-top: -4px;" :icon="['fas', 'sort-down']" />
+                        <font-awesome-icon v-else class="ms-1 sortIcon" style="margin-top: 8px;" :icon="['fas', 'sort-up']" />
+                    </div>
                 </div>
-                <div class="fw-bold" style="width: 20%">
-                    Type
+                <div class="d-flex justify-content-around" style="width: 20%">
+                    
+                    <div class="d-flex align-items-center fw-bold cur-p px-2 py-1 sortItem" @click="toggleSort('typeSort')">
+                        Type
+                        <font-awesome-icon v-if="typeSort" class="ms-1 sortIcon" style="margin-top: -4px;" :icon="['fas', 'sort-down']" />
+                        <font-awesome-icon v-else class="ms-1 sortIcon" style="margin-top: 8px;" :icon="['fas', 'sort-up']" />
+                    </div>
                 </div>
                 <div class="fw-bold" style="width: 30%">
                     Details
                 </div>
-                <div class="fw-bold" style="width: 10%">
-                    State
+                <div class="d-flex justify-content-around" style="width: 10%">
+                    <div class="d-flex align-items-center fw-bold cur-p px-2 py-1 sortItem" @click="toggleSort('stateSort')">
+                        State
+                        <font-awesome-icon v-if="stateSort" class="ms-1 sortIcon" style="margin-top: -4px;" :icon="['fas', 'sort-down']" />
+                        <font-awesome-icon v-else class="ms-1 sortIcon" style="margin-top: 8px;" :icon="['fas', 'sort-up']" />
+                    </div>
                 </div>
                 <div class="fw-bold" style="width: 10%">
                     Action
                 </div>
             </div>
-            <div v-for="item in items" :key="item._id">
+            <div v-if="items.length == 0" style="height: 300px">
+                <div class="d-flex fs-1 justify-content-center fw-bold align-items-center h-100">NO VOUCHER FOUND</div>
+            </div>
+            <div v-else v-for="item in items" :key="item._id">
                 <div class="bg-white border px-3 py-2 mb-3 rounded">
                     <div class="vstack">
                         <div class="d-flex justify-content-between text-center align-items-center">
@@ -50,7 +74,7 @@
                             </div>
                             <div style="width: 20%">
                                 <div class="d-flex justify-content-around" >
-                                    <div class="fw-medium rounded px-3 py-1 bg-secondary text-white" style="cursor: default">
+                                    <div class="fw-medium rounded px-3 py-1 bg-secondary text-white cur-d">
                                         {{ item.type }}
                                     </div>
                                 </div>
@@ -60,13 +84,13 @@
                             </div>
                             <div style="width: 10%">
                                 <div class="d-flex justify-content-around" >
-                                    <div class="fw-medium rounded px-3 py-1" :class="getCssState(item.state)" style="cursor: default">
+                                    <div class="fw-medium rounded px-3 py-1 cur-d" :class="getCssState(item.state)">
                                         {{ item.state }}
                                     </div>
                                 </div>    
                             </div>
-                            <div class="d-flex justify-content-center"  style="width: 10%">
-                                <div class="d-flex">
+                            <div  class="d-flex justify-content-center"  style="width: 10%">
+                                <div v-if="check" class="d-flex">
                                     <button class="rounded px-3 py-1 bg-primary text-white">
                                         <font-awesome-icon :icon="['fas', 'pen']" />
                                     </button>
@@ -114,32 +138,43 @@ export default {
             page: 1,
             pages: 0,
             search: "",
+            check: true,
+            startDateSort: true,
+            typeSort: true,
+            stateSort: true,
+            sort: ["startDateSort", "typeSort", "stateSort"],
             items: []
         }
     },
+    computed: {
+        labelSwitcher() {
+            return this.check === true ? "" : "not "
+        },
+        jsonSort() {
+            var sort = this.sort.reduce((obj, sortItem) => {
+                obj[sortItem.replace("Sort", "")] = this[sortItem]
+                return obj
+            }, {})
+            return JSON.stringify(sort)
+        }
+    },
     created() {
-        this.fetchItems(this.page, this.search)
-        this.getPages(this.search)
+        this.fetchItemsAndGetPages({})
     },
     watch: {
         page(newPage) {
-            this.fetchItems(newPage, this.search)
+            this.fetchItemsAndGetPages({ pageValue: newPage })
         },
         search(newSearch) {
             this.page = 1
-            this.fetchItems(this.page, newSearch)
-            this.getPages(newSearch)
+            this.fetchItemsAndGetPages({ pageValue: 1 ,searchValue: newSearch})
+        },
+        check(newCheck) {
+            this.page = 1
+            this.fetchItemsAndGetPages({ pageValue: 1,  checkValue: newCheck})
         }
     },
     methods: {
-        fetchItems(pageValue, searchValue) {
-            VoucherService.getByPage(pageValue, searchValue)
-                .then((res) => this.items = res.data)
-                .catch((err) => console.log(err))
-        },
-        getDateString(value) {
-            
-        },
         getCssState(state) {
             var cssString = []
             switch (state) {
@@ -167,11 +202,49 @@ export default {
             }
             return cssString.join(" ")
         },
-        getPages(searchValue) {
-            VoucherService.getPages(searchValue)
+        fetchItems(pageValue, searchValue, checkValue, sortValue) {
+            VoucherService.getByPage(pageValue, searchValue, checkValue, sortValue)
+                .then((res) => this.items = res.data)
+                .catch((err) => console.log(err))
+        },
+        getPages(searchValue, checkValue) {
+            VoucherService.getPages(searchValue, checkValue)
                 .then((res) => this.pages = res.data.count )
                 .catch((err) => console.log(err))
+        },
+        fetchItemsAndGetPages({pageValue = this.page, searchValue = this.search, checkValue = this.check}) {
+            this.fetchItems(pageValue, searchValue, checkValue, this.jsonSort)
+            this.getPages(searchValue, checkValue)
+        },
+        
+        toggleSort(sortValue) {
+            this[sortValue] = !this[sortValue]
+            if (this.sort.indexOf(sortValue) > -1) {
+                this.sort.splice(this.sort.indexOf(sortValue), 1)
+                this.sort.unshift(sortValue)
+            }
+            console.log(this.page)
+            this.fetchItemsAndGetPages({})
         }
     }
 }
 </script>
+
+<style scoped>
+button {
+    position: relative;
+    transition: .2s
+}
+button:hover {
+    transform: translateY(-2px);
+}
+button:active {
+    transform: translateY(-1px)
+}
+.sortIcon {
+    transition: .1s;
+}
+.sortItem:hover .sortIcon{
+    color: rgb(155, 155, 155)
+}
+</style>
