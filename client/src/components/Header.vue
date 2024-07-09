4c7380ba0930d562160c60125135412ac82a9d41
Bich Ngoc <nbichngoc3904@gmail.com>
2024-07-09 20:41:37 +0700

    Đổi giao diện Login,Register

---
+++ client/src/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="vstack gap">
+        <div class="header-top">
+            <h1>Ảnh nav ở đây</h1>
+        </div>
+
+        <div class="header-mid">
+            <h1>Logo và tìm kiếm</h1>
+        </div>
+
+        <div class="header-menu">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Navbar</a>
+                  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                      </li>
+                    </ul>
+                    <form class="d-flex">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+        </div>
+
+
+
+    </div>
+    
+
+
+
+
+
+    
+</template>
+<style>
+    :root {
+        --text: #212425;
+        --nav: #225771;
+        --orange: #EF8121;
+        --text: #021534;
+    }
+
+</style>
--- client/src/views/Test.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>Hello test nè</h1>
-    <h1>Test mới nè</h1>
-</template>
