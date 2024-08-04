7529f433fc6410df0f42eecfd4e7ba20fc68d8dd
PhanTrongPhuc <phantrongphuc12004@gmail.com>
2024-08-04 22:10:29 +0700

    final

--- server/routes/VoucherRoute.js
+++ server/routes/VoucherRoute.js
@@ -8,6 +8,7 @@ VoucherRouter.get("/exist/:code", VoucherController.isExist)
 VoucherRouter.get("/randomcode", VoucherController.getRandomCode)
 VoucherRouter.get("/type", VoucherController.getVoucherType)
 VoucherRouter.get("/find/:code", VoucherController.findVoucher)
+VoucherRouter.get('/findById/:id', VoucherController.findById);
 VoucherRouter.delete("/:id", VoucherController.deleteVoucher)
 VoucherRouter.post("/create", VoucherController.createVoucher)
 VoucherRouter.post("/update/:code", VoucherController.updateVoucher)
